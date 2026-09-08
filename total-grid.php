aa890cbb0e20c2ac31eb838f97f0e00ef8cc7413
kawmin <kawmin207@gmail.com>
2026-09-08 22:39:55 +0600

    Add total grid page

---
+++ total-grid.php
@@ -0,0 +1,22 @@
+<?php
+
+
+require_once __DIR__ . "/includes/authentic.php";
+
+
+require_once __DIR__ . "/app/controllers/GovernmentController.php";
+
+
+
+
+
+$controller = new GovernmentController();
+
+
+
+
+$controller->totalGrid();
+
+
+
+?>
