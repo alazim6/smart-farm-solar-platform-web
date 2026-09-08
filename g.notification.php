bb9dad10c0c429de63dc1390d4f2635d9e594619
kawmin <kawmin207@gmail.com>
2026-09-08 22:38:56 +0600

    Add government notification page

---
+++ g.notification.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "includes/authentic.php";
+
+require_once "app/controllers/GovernmentController.php";
+
+
+$controller = new GovernmentController();
+
+
+$controller->notification();
+
+
+?>
