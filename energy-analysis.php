b24b8706554f332e3bda16673bf9c1b6316a9ef0
kawmin <kawmin207@gmail.com>
2026-09-08 22:39:28 +0600

    Add energy analysis page

---
+++ energy-analysis.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require_once __DIR__ . "/includes/authentic.php";
+
+
+require_once __DIR__ . "/app/controllers/GovernmentController.php";
+
+
+
+
+$controller = new GovernmentController();
+
+
+
+
+$controller->energyAnalysis();
+
+
+
+?>
